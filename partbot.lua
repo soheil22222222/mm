e81a807a7fcc15f62f6b110a55b4913892d0574d
sargardan <soheil22222222@users.noreply.github.com>
2016-06-23 20:01:43 +0430

    Update and rename bot/shieldbot.lua to partbot.lua

--- partbot.lua
+++ partbot.lua
@@ -248,8 +248,6 @@ function create_config( )
     "send",
     "set_type",
     "welcome",
-    "sh",
-    "serverinfo"
     },
     sudo_users = {166053947},--Sudo users
     moderation = {data = 'data/moderation.json'},
@@ -325,116 +323,6 @@ function create_config( )
 
 میتوانید از هردوی کاراکتر های { /و ! } برای دستورات استفاده کنید
 
-︿﹀︿﹀︿﹀︿﹀︿﹀︿﹀
-
-@Hacker_Team
-]],
-    help_text = [[
-English commands:
-
-︽︾︽︾︽︾︽︾︽︾︽︾︽︾
-⭕️ /kick [username/id]
-You can also do it by reply
-
-⭕️ /ban [username/id]
-You can also do it by reply
-
-⭕️ /unban [id]
-You can also do it by reply
-
-⭕️ /who
-Members list
-
-⭕️ /modlist
-Moderators list
-
-⭕️ /promote [username]
-Promote someone
-
-⭕️ /demote [username]
-Demote someone
-
-⭕️ /kickme
-Will kick user
-
-⭕️ /about
-Group description
-
-⭕️ /setphoto
-Set and locks group photo
-
-⭕️ /setname [name]
-Set group name
-
-⭕️ /rules
-Group rules
-
-⭕️ /id
-Return group id or user id
-
-⭕️ /lock
- [member|name|bots|leave] 
-
-⭕️ /Locks
- [member|name|bots|leaveing] 
-
-⭕️ /unlock
- [member|name|bots|leave]
-
-⭕️ /Unlocks
- [member|name|bots|leaving]
-
-⭕️ /set rules [text]
-Set [text] as rules
-
-⭕️ /set about [text]
-Set [text] as about
-
-⭕️ /settings
-Returns group settings
-
-⭕️ /newlink
-Create/revoke your group link
-
-⭕️ /link
-Returns group link
-
-⭕️ /owner
-Returns group owner id
-
-⭕️ /setowner [id]
-Will set id as owner
-
-⭕️ /info [reply/username/none]
-returns
-
-⭕️ /setflood [value]
-Set [value] as flood sensitivity
-
-⭕️ /stats
-Simple message statistics
-
-⭕️ /save [value] [text]
-Save [text] as [value]
-
-⭕️ /get [value]
-Returns text of [value]
-
-⭕️ /clean [modlist|rules|about]
-Will clear [modlist|rules|about] and set it to nil
-
-⭕️ /res [username]
-Returns user id
-
-⭕️ /log
-Will return group logs
- 
-⭕️ /banlist
-Will return group ban list
-
-
-︿﹀︿﹀︿﹀︿﹀︿﹀︿﹀
-☎️Send /share to get robot number
 ︿﹀︿﹀︿﹀︿﹀︿﹀︿﹀
 
 @Hacker_Team
